0d2c4d8156fdbb847f83a7ededa5390f86ba0b0f
Joshua Mabawonku <drakejoshua086@gmail.com>
2026-03-20 23:40:49 +0100

    db: Update UserSchema.js and add supportedCurrencies.js
    This commit includes updates to the UserSchema.js file, adding a
    new fields for user data such as: name, email, password, auth metadata.
    Additionally, a new file named supportedCurrencies.js has been created
    in the Database/utils directory to manage supported currency options for
    the application. These changes enhance the user authentication process
    and provide a structured way to handle currency data within the application.

--- Database/Schema/UserSchema.js
+++ Database/Schema/UserSchema.js
@@ -1,7 +1,52 @@
 import mongoose from "mongoose";
+import { supportedCurrencies } from "../utils/supportedCurrencies.js";
 
+// User schema to represent user data in the database with validation rules and default values
 const UserSchema = mongoose.Schema({
-    
+    name: {         // validation rules for name field: required, min length of 3 characters
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    email: {        // validation rules for email field: required, unique, and must match email regex pattern
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function( value ) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test( value )
+            },
+            message: props => `${ props.value } is not a valid email address`
+        }
+    },
+    password: {        // validation rules for password field: required and min length of 6 characters
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    profile_photo: String,
+    profile_photo_public_id: String,
+    currency: {         // validation rules for currency field: must be one of the supported currencies and defaults to USD
+        type: String,
+        enum: supportedCurrencies,
+        default: "USD"
+    },
+    portfolio: [ AssetSchema ],
+    watchlist: [ String ],
+    email_verified: {       // validation rules for email_verified field: boolean that defaults to false
+        type: Boolean,
+        default: false
+    },
+    email_verification_token: String,
+    email_verification_expires: Date,
+    password_reset_token: String,
+    password_reset_expires: Date
+})
+
+// Asset schema to represent individual assets in the user's portfolio
+const AssetSchema = mongoose.Schema({
+    symbol: String,
+    units: Number
 })
 
 
---
+++ Database/utils/supportedCurrencies.js
@@ -0,0 +1,15 @@
+// This file contains a list of supported currencies for displaying values in the application.
+
+/* 
+    Supported Currencies:
+    - USD: United States Dollar
+    - EUR: Euro
+    - GBP: British Pound Sterling
+    - JPY: Japanese Yen
+    - AUD: Australian Dollar
+    - CAD: Canadian Dollar
+    - NGN: Nigerian Naira
+    - INR: Indian Rupee
+*/
+
+export const supportedCurrencies = ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "NGN", "INR"]
