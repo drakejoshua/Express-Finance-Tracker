313baacb689c004eb3fff28122adc35d2698101f
Joshua Mabawonku <drakejoshua086@gmail.com>
2026-04-02 13:35:35 +0100

    feat( portfolio ): add "update portfolio asset" endpoint/functionality
    This commit adds a new API endpoint for updating an existing asset in the
    user's portfolio. The endpoint allows users to modify the number of units they hold.
    This enhancement provides users with greater flexibility in managing their portfolio and
    keeping their asset information up to date.

--- Portfolio/routes.js
+++ Portfolio/routes.js
@@ -160,4 +160,92 @@ router.delete("/:symbol",
 )
 
 
+// PUT /app/portfolio/:symbol - update the quantity of an asset in the authenticated user's portfolio 
+// using the asset symbol provided in the request params and the new quantity provided in the request body.
+// Requires a valid JWT token in the Authorization header.
+router.put("/:symbol",
+    [
+        param("symbol")
+            .exists()
+            .withMessage( ERROR_CODES.INVALID_ASSET_SYMBOL)
+            .bail()
+            .notEmpty()
+            .withMessage( ERROR_CODES.INVALID_ASSET_SYMBOL)
+            .bail(),
+        body("units")
+            .exists()
+            .withMessage( ERROR_CODES.INVALID_ASSET_UNITS )
+            .bail()
+            .notEmpty()
+            .withMessage( ERROR_CODES.INVALID_ASSET_UNITS )
+            .bail()
+            .isInt({ gt: 0 })
+            .withMessage( ERROR_CODES.INVALID_ASSET_UNITS )
+            .bail(),
+        header("Authorization")
+            .exists()
+            .withMessage( ERROR_CODES.INVALID_AUTHORIZATION_TOKEN )
+            .bail()
+            .notEmpty()
+            .withMessage( ERROR_CODES.INVALID_AUTHORIZATION_TOKEN )
+            .bail()
+            .custom( validateBearerJWT )
+            .withMessage( ERROR_CODES.INVALID_AUTHORIZATION_TOKEN )
+            .bail()
+    ],
+    function( req, res, next ) {
+        // extract validation errors from the request if any
+        const errors = validationResult( req )
+
+        // check if there are validation errors and return the 
+        // first error message if any
+        if ( !errors.isEmpty() ) {
+            switch ( errors.array()[0].msg ) {
+                case ERROR_CODES.INVALID_ASSET_SYMBOL:
+                    return reportInvalidAssetSymbolError( next )
+                case ERROR_CODES.INVALID_AUTHORIZATION_TOKEN:
+                    return reportInvalidAuthorizationTokenError( next )
+                case ERROR_CODES.INVALID_ASSET_UNITS:
+                    return reportInvalidAssetUnitsError( next )
+            }
+        }
+
+        // proceed with the request handling if there are no validation errors
+        next()
+    },
+    passport.authenticate("jwt", { session: false }),
+    async function( req, res, next ) {
+        try {
+            // get the authenticated user from the request object 
+            // (populated by passport after successful authentication)
+            const authenticatedUser = req.user
+
+            // extract the asset symbol from the request params and the 
+            // new quantity from the request body
+            const { symbol } = req.params
+            const { units } = req.body
+
+            // check if the asset exists in the user's portfolio before trying to update it,
+            // and return an error if not found
+            if ( !authenticatedUser.portfolio.find( ( item ) => item.symbol === symbol ) ) {
+                return reportAssetNotFoundError( next )
+            }
+
+            // update the asset quantity in the user's portfolio in the database
+            await authenticatedUser.updateAssetToPortfolio( symbol, units )
+
+            // return a success response with the updated portfolio data
+            return res.json({
+                status: "success",
+                data: {
+                    message: `Successfully updated ${ symbol } in portfolio.`
+                }
+            })
+        } catch ( error ) {
+            return reportPortfolioOperationFailureError( next, error.message )
+        }
+    }
+)
+
+
 export default router
